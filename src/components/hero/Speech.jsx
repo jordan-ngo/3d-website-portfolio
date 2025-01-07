fa0665a5b8e714a3ca47fb7c7a9412c7612bd602
jordan-ngo <ngojord@gmail.com>
2025-01-07 10:19:13 -0800

    Updated motion syntax

--- src/components/hero/Speech.jsx
+++ src/components/hero/Speech.jsx
@@ -1,8 +1,12 @@
 import { TypeAnimation } from 'react-type-animation';
+import { motion } from "motion/react";
 
 const Speech = () => {
     return (
-        <div className="bubbleContainer">
+        <motion.div className="bubbleContainer" 
+        animate={{opacity:[0, 1]}} 
+        transition={{duration: 1}}
+    >
             <div className="bubble">
                 <TypeAnimation
                     sequence={[
@@ -18,7 +22,7 @@ const Speech = () => {
                     repeat={Infinity}
                 /></div>
             <img src="/man.png" alt="" />
-        </div>
+        </motion.div>
     );
 };
 
