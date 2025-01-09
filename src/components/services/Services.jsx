82bb890b221b7e4f32e272f93238403ea6f3ebf8
jordan-ngo <ngojord@gmail.com>
2025-01-09 15:10:12 -0800

    Pushing changes for Services.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -4,7 +4,7 @@ const Services = () => {
   return (
     <div className='services'>
       <div className="sSection left"></div>
-      <div className="sSection right"></div>
+      <div className="Section right"></div>
     </div>
   )
 }
