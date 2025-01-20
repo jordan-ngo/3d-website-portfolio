d8a965c467bc4368a82b079159b377ce3c22ce1d
jordan-ngo <ngojord@gmail.com>
2025-01-20 10:59:08 -0800

    Updated indentation in ComputerModelContainer.jsx

--- src/components/services/computer/ComputerModelContainer.jsx
+++ src/components/services/computer/ComputerModelContainer.jsx
@@ -1,19 +1,20 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
 import {ComputerModel} from "./ComputerModel";
-import { OrbitControls, Stage } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera, Stage } from "@react-three/drei";
 
 const ComputerModelContainer = () => {
   return (
     <Canvas>
         <Suspense fallback="loading...">
-          <Stage environment="city">
+          <Stage environment="night" intensity={0.5}>
             <ComputerModel />
           </Stage>
-            <OrbitControls enableZoom={false}/>
+            <OrbitControls enableZoom={false} autoRotate/>
+            <PerspectiveCamera position={[-1, 0, 0.8]} zoom={0.8} makeDefault/>
         </Suspense>
     </Canvas>
   );
 };
 
-export default ComputerModelContainer
+export default ComputerModelContainer;
