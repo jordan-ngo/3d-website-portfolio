71e603cf1881d0400f3c21dc789c7935955db1b5
jordan-ngo <ngojord@gmail.com>
2025-01-20 14:21:14 -0800

    Added logic for services page

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,16 +1,90 @@
-import "./services.css"
 import ComputerModelContainer from "./computer/ComputerModelContainer";
 import ConsoleModelContainer from "./console/ConsoleModelContainer";
 import MugModelContainer from "./mug/MugModelContainer";
+import "./services.css";
+import Counter from "./Counter";
+import { motion } from "motion/react";
+
+const textVaraints = {
+  initial: {
+    x: -100,
+    y: -100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1
+    }
+  }
+};
+
+const listVariants = {
+  initial: {
+    x: -100,
+    y: -100,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+const services = [
+  {
+    id: 1,
+    img: "/service1.png",
+    title: "Web Development",
+    counter: 35,
+  },
+  {
+    id: 2,
+    img: "/service2.png",
+    title: "Product Design",
+    counter: 23,
+  },
+  {
+    id: 3,
+    img: "/service3.png",
+    title: "Branding",
+    counter: 46,
+  }
+];
 
 const Services = () => {
   return (
-    <div className='services'>
-      <div className="sSection left"></div>
-      <div className="sSection right"></div>
-        <MugModelContainer/>
+    <div className="services">
+      <div className="sSection left">
+        <h1 className="sTitle">How do I help?</h1>
+        <div className="serviceList">
+          {services.map((service) => (
+            <div className="service" key={service.id}>
+              <div className='serviceIcon'>
+                <img src={service.img} alt="" />
+              </div>
+              <div className="serviceInfo">
+                <h2>{service.title}</h2>
+                <h3>{service.counter} Projects</h3>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="counterList">
+          <Counter from={0} to={104} text="Projects Completed" />
+          <Counter from={0} to={72} text="Happy Clients" />
+        </div>
+      </div>
+      <div className="sSection right"> {/* <MugModelContainer/> */} </div>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
