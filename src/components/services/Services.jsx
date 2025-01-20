0ae158c014d823503b180be8be4f4a8a7cc857a3
jordan-ngo <ngojord@gmail.com>
2025-01-20 10:58:31 -0800

    Updated Services.jsx to support new 3d models

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,12 +1,14 @@
 import "./services.css"
 import ComputerModelContainer from "./computer/ComputerModelContainer";
+import ConsoleModelContainer from "./console/ConsoleModelContainer";
+import MugModelContainer from "./mug/MugModelContainer";
 
 const Services = () => {
   return (
     <div className='services'>
       <div className="sSection left"></div>
       <div className="sSection right"></div>
-        <ComputerModelContainer/>
+        <MugModelContainer/>
     </div>
   )
 }
