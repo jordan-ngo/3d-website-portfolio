f34e2863f876e9346f32874bd6f2c63be97887e1
jordan-ngo <ngojord@gmail.com>
2025-01-21 14:22:50 -0800

    Updated Contact.jsx and Contact.css for content and styling

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,69 @@
 import "./contact.css"
+import emailjs from '@emailjs/browser';
+import { useRef, useState } from "react";
 
 const Contact = () => {
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState(false);
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE.TEMPLATE.ID,
+        e.target,
+        {
+          publicKey: import.meta.env.VITE_PUBLIC_KEY,
+        })
+      .then(
+        () => {
+          setSuccess(true);
+          setError(false);
+        },
+        (error) => {
+          setError(true);
+          setSuccess(false);
+        },
+      );
+  };
+
   return (
-    <div className='contact'>Contact</div>
+    <div className='contact' onSubmit={sendEmail}>
+      <div className="cSection">
+        <form ref={form}>
+          <h1 className="cTitle">Let's keep in touch!</h1>
+          <div className="formItem">
+            <label>Name</label>
+            <input
+              type="text"
+              name="user_username"
+              placeholder="John Doe" />
+          </div>
+          <div className="formItem">
+            <label>Email</label>
+            <input
+              type="email"
+              name="user_email"
+              placeholder="john@gmail.com" />
+          </div>
+          <div className="formItem">
+            <label>Message</label>
+            <textarea
+              rows={10}
+              name="user_message"
+              placeholder="Write your message..."
+            ></textarea>
+          </div>
+          <button className="formButton">Send</button>
+          {success && <span>Your message has been sent!</span>}
+          {error && <span>Something went wrong!</span>}
+        </form>
+      </div>
+      <div className="cSection">SVG</div>
+    </div>
   )
 }
 
